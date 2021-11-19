627ba63383141a5c4ca9635fcd06b1b9c9a70fd1
Leandro A. Siqueira <leandroaps@gmail.com>
2021-11-19 10:43:46 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useMachine } from '@xstate/react';
+import { trafficLightMachine } from './trafficLightMachine';
 
-function App() {
+export const App = () => {
+  // Typescript will infer what current and send are here
+  // And will provide useful information about usage
+  const [current, send] = useMachine(trafficLightMachine);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="pole" />
+      <div className="traffic-light">
+        <input
+          type="radio"
+          readOnly
+          className="light red"
+          checked={current.matches('red')}
+        />
+        <input
+          type="radio"
+          readOnly
+          className="light yellow"
+          checked={current.matches('yellow')}
+        />
+        <input
+          type="radio"
+          readOnly
+          className="light green"
+          checked={current.matches('green')}
+        />
+        <button onClick={() => send('NEXT')}>NEXT</button>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
